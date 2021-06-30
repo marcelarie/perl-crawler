4d598b1aa0dafc47f7be39dbc90183270304f054
Marcel Arie <googlillo@gmail.com>
2021-06-30 20:07:02 +0200

    build: base crawler

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # perl-crawler
+
+Perl crawler of [Coinpare](https://coinpare.io/) with the
+[Mojolicius](https://mojolicious.org/) web framework.
+
+### TODO
+
+1. Extract top 100 cryptos -> https://coinpare.io/ on GET '/'
+2. Search by coin name/initials
---
+++ myapp.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite -signatures;
+
+plugin 'AutoReload';
+
+use Mojo::UserAgent;
+
+# Fine grained response handling (dies on connection errors)
+my $ua = Mojo::UserAgent->new;
+
+# Render template "index.html.ep" from the DATA section
+get '/' => sub ($c) {
+
+    # if    ( $res->is_success )  { say $res->body }
+    # elsif ( $res->is_error )    { say $res->message }
+    # elsif ( $res->code == 301 ) { say $res->headers->location . 301 }
+    # else                        { say 'Whatever...' }
+
+    my $crypto_url = 'https://coinpare.io/';
+    my $res        = $ua->get($crypto_url)->result;
+
+    my @coins = $res->dom->find('td.coinName a')->map('text');
+
+    $c->render( json => { coins => @coins } );
+};
+
+app->start;
+__DATA__
+
+@@ index.html.ep
+% my $url = url_for 'title';
+<html>
+    <body>$res</body>
+</html>
