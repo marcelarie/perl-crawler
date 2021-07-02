45e56cd7e5e540b54bf99cbf0686330c6c62ac83
Marcel Arie <googlillo@gmail.com>
2021-07-02 16:50:40 +0200

    add: hash map return with name and price

--- myapp.pl
+++ myapp.pl
@@ -17,13 +17,24 @@ get '/' => sub ($c) {
     # elsif ( $res->code == 301 ) { say $res->headers->location . 301 }
     # else                        { say 'Whatever...' }
 
-    my $crypto_url = 'https://coinpare.io/';
-    my $res        = $ua->get($crypto_url)->result;
+    my $crypto_url = 'https://coinpare.io/?cP=';
+    my $page       = 1;
+    my $res        = $ua->get( $crypto_url . $page )->result;
 
-    my @coins =
-      $res->dom->find('td.coinName a')->map('text')->map( sub { s/(\t)|(\n)|(\r)//gr } );
+    my @names =
+      $res->dom->find('td.coinName a')->map('text')
+      ->map( sub { s/(\t)|(\n)|(\r)//gr } );
 
-    $c->render( json => { coins => @coins } );
+    my @prices = $res->dom->find('td.tPriceW')->map('text')
+      ->map( sub { s/(\t)|(\n)|(\r)//gr } );
+
+    my %hash = ();
+    for ( 0 .. 99 ) {
+        $hash{ $_ + 1 } =
+          { name => @names[0]->[$_], price_in_dollar => @prices[0]->[$_] };
+    }
+
+    $c->render( json => {%hash} );
 };
 
 app->start;
